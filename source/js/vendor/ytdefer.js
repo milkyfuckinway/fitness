da1490957c67932faeb023011b41532408815890
milkyfuckinway <0-0_0@bk.ru>
2023-03-29 13:12:45 +0300

    fix

--- source/js/vendor/ytdefer.js
+++ source/js/vendor/ytdefer.js
@@ -1,46 +1,51 @@
 function ytdeferSetup() {
-  let d = document;
-  let els = d.getElementsByClassName('ytdefer');
-  for (let i = 0; i < els.length; i++) {
-    let e = els[i];
-    let dv = d.createElement('div');
-    dv.id = 'ytdefer_vid' + i;
-    dv.style.position = 'relative';
-    e.appendChild(dv);
-    let image = d.createElement('img');
-    image.id = 'ytdefer_img' + i;
-    image.onclick = genYtdeferClk(i);
-    dv.appendChild(image);
-    let button = d.createElement('button');
-    button.id = 'ytdefer_icon' + i;
-    button.setAttribute('aria-label', 'Воспроизвести.');
-    dv.appendChild(button);
-  }
-
+  const ytdeferElements = document.querySelectorAll('.ytdefer');
+  ytdeferElements.forEach((ytdeferElement, index) => {
+    const divElement = createDivElement(index);
+    ytdeferElement.appendChild(divElement);
+    const buttonElement = createButtonElement(index);
+    buttonElement.addEventListener('click', genYtdeferClk(index));
+    divElement.appendChild(buttonElement);
+  });
   if (typeof YT === 'undefined') {
-    let js = d.createElement('script');
-    js.type = 'text/javascript';
-    js.src = 'https://www.youtube.com/player_api';
-    d.body.appendChild(js);
+    loadYouTubePlayerAPI();
   }
 }
 
-function genYtdeferClk(i) {
-  return function () {
-    let d = document;
-    let el = d.getElementById('ytdefer_vid' + i);
-    let vidId = el.parentNode.getAttribute('data-src');
-    // eslint-disable-next-line no-undef, no-unused-vars
-    let player = new YT.Player(el.id, {
-      videoId: vidId,
-      events: {
-        onReady(ev) {
-          ev.target.playVideo();
-        },
-      },
-    });
-  };
+function createDivElement(index) {
+  const divElement = document.createElement('div');
+  divElement.id = `ytdefer_vid${index}`;
+  divElement.style.position = 'relative';
+  return divElement;
 }
+
+function createButtonElement(index) {
+  const buttonElement = document.createElement('button');
+  buttonElement.id = `ytdefer_icon${index}`;
+  buttonElement.setAttribute('aria-label', 'Воспроизвести.');
+  return buttonElement;
+}
+
+function loadYouTubePlayerAPI() {
+  const scriptElement = document.createElement('script');
+  scriptElement.type = 'text/javascript';
+  scriptElement.src = 'https://www.youtube.com/player_api';
+  document.body.appendChild(scriptElement);
+}
+
+const genYtdeferClk = (index) => () => {
+  const videoDivElement = document.querySelector(`#ytdefer_vid${index}`);
+  const videoId = videoDivElement.parentNode.getAttribute('data-src');
+  // eslint-disable-next-line no-undef, no-unused-vars
+  const player = new YT.Player(videoDivElement.id, {
+    videoId,
+    events: {
+      onReady(event) {
+        event.target.playVideo();
+      },
+    },
+  });
+};
 
 const ytdeferInit = () => {
   ytdeferSetup();
